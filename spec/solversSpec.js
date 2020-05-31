78ae444e090a48e319afcb92d5c8949ab3fc3652
Ahmed Zoghayyer <azzoghayyer@gmail.com>
2020-05-31 00:57:23 -0700

    Remove extra space

--- spec/solversSpec.js
+++ spec/solversSpec.js
@@ -31,7 +31,6 @@ describe('solvers', function() {
   });
 
   describe('findNQueensSolution()', function() {
-
     it('finds a valid solution for n of 0-7', function() {
       // Skip 2 and 3 because they have no solution.
       [1, 4, 5, 6, 7, 8].map(function(n) {
